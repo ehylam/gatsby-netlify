c34f97885081548a706008ec2e3cd6bee38bb343
Eric Lam <ehylam.p@gmail.com>
2020-05-05 15:11:25 +1000

    more widgets

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,29 @@ import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 
-const IndexPage = () => (
-  <Layout>
 
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+
+
+const IndexPage = () => {
+
+  return (
+    <Layout>
+
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
 
+export const indexQuery = graphql`
+  query IndexPageTemplate {
+    markdownRemark {
+      frontmatter {
+        featuredImage
+        heading
+        title
+      }
+    }
+  }
+`
