7d672fb04212a10cec614bad70ece22c0119d437
Eric Lam <ehylam.p@gmail.com>
2020-05-06 13:51:10 +1000

    Fixed errors.

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,16 +7,16 @@ import Layout from "../components/layout"
 
 
 const IndexPage = ({data}) => {
-  const {markdownRemark}  = data;
-  const {frontmatter, html } = markdownRemark;
-  console.log(markdownRemark);
-  console.log(frontmatter);
-  console.log(html);
+  // const {markdownRemark}  = data;
+  // const {frontmatter, html } = markdownRemark;
+  // console.log(markdownRemark);
+  // console.log(frontmatter);
+  // console.log(html);
   return (
     <Layout>
-
+{/*
       <img src={frontmatter.featuredImage}></img>
-      <h1>{frontmatter.heading}</h1>
+      <h1>{frontmatter.heading}</h1> */}
       <Link to="/page-2/">Go to page 2</Link>
     </Layout>
   )
@@ -24,17 +24,17 @@ const IndexPage = ({data}) => {
 
 export default IndexPage
 
-export const indexQuery = graphql`
-  query IndexPageTemplate {
-    markdownRemark {
-      frontmatter {
-        featuredImage
-        heading
-        title
-        subheading
-        date
-      }
-      html
-    }
-  }
-`
+// export const indexQuery = graphql`
+//   query IndexPageTemplate {
+//     markdownRemark {
+//       frontmatter {
+//         featuredImage
+//         heading
+//         title
+//         subheading
+//         date
+//       }
+//       html
+//     }
+//   }
+// `
