db90dddaa566f3a1df6380ed6fb8bb61cf5340ba
Eric Lam <ehylam.p@gmail.com>
2020-05-06 13:53:55 +1000

    gatsby plugin fix test

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `markdown-pages`,
-        path: `${__dirname}/src/pages/contents`
+        path: `${__dirname}/src/pages/`
       }
     },
     `gatsby-transformer-remark`,
