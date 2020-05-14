50d471365abbe183d4e2708fa946bb9298c763ef
Eric Lam <ehylam.p@gmail.com>
2020-05-14 14:39:11 +1000

    Fixed issue, hopefully

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,17 +3,19 @@ import React from 'react';
 
 const hero = ({data}) => {
   return (
-    data.map((content) => (
-<section className="hero_container" >
-  <div className="background" style="background-image: url()">
-    <div className="hero-content__wrapper">
-      <h1 className="me">{content.heading}</h1>
-      <h3>{content.description}</h3>
-    </div>
-  </div>
-  <div className="foreground"></div>
-</section>
-    ))
+    <section className="hero_container" >
+      {data.map((content) => (
+        <div className="hero-wrapper" >
+          <div className="background" style="background-image: url()">
+            <div className="hero-content__wrapper">
+              <h1 className="me">{content.heading}</h1>
+              <h3>{content.description}</h3>
+            </div>
+          </div>
+          <div className="foreground"></div>
+        </div>
+      ))}
+    </section>
    );
 }
 
